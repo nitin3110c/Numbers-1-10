670a8601199b878f62256d3442f23420a0c589e7
Nitin Nagesh <42811436+nitin3110c@users.noreply.github.com>
2018-08-29 23:32:25 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,24 @@
+public class ABC
+{
+	public static void main(String[]args)
+	{
+
+		{
+			for(int i=1;i<=10;i++)
+			System.out.println(i);
+		}
+				int i=0;
+				while(i<10)
+		{
+				i++;
+			System.out.println(i);
+		}
+				int j=0;
+				do
+				{
+					System.out.println(j);
+					t++;
+				}
+				while(j<10);
+	}
+}
